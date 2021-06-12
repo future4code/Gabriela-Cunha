46efe0e23131f17a8dffb0a763743ef8a39e93b0
Gabriela Fogaça <gabifogaca.gabriela@gmail.com>
2021-06-12 00:16:06 -0300

    Entrega do projeto quinzenal da semana 3 e 4

--- semana3/projetoQuinzena2/nossoJogo.js
+++ semana3/projetoQuinzena2/nossoJogo.js
@@ -11,47 +11,31 @@
  * 
  */
 
-
-console.log('Bem vindos ao jogo de BlackJack!')
-let jogar = confirm('Quer iniciar uma nova rodada?')
-
-if (jogar){
-
-   let usuarioCarta1 = comprarCarta()
-   let usuarioCarta2 = comprarCarta()
-   let computadorCarta1 = comprarCarta()
-   let computadorCarta2 = comprarCarta()
-
-   console.log(`Usuário: ${usuarioCarta1.texto} e ${usuarioCarta2.texto}. Pontuação: ${usuarioCarta1.valor + usuarioCarta2.valor}`)
-   console.log(`Computador: ${computadorCarta1.texto} e ${computadorCarta2.texto}. Pontuação: ${computadorCarta1.valor + computadorCarta2.valor}`)
-   if(usuarioCarta1.valor + usuarioCarta2.valor > computadorCarta1.valor + computadorCarta2.valor){
-      console.log(`O usuário ganhou!`)
-
-   }else if(computadorCarta1.valor + computadorCarta2.valor > usuarioCarta1.valor + usuarioCarta2.valor){
-      console.log(`O computador ganhou!`)
-
-   }else{
-      console.log(`Empate!`)
-   }
-}else{
-   console.log(`O jogo acabou! ~Nem começou na verdade kkk~`)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log('Bem vindos ao jogo de BlackJack!')
+    let jogar = confirm('Quer iniciar uma nova rodada?')
+    
+    if (jogar){
+    
+       let usuarioCarta1 = comprarCarta()
+       let usuarioCarta2 = comprarCarta()
+       let computadorCarta1 = comprarCarta()
+       let computadorCarta2 = comprarCarta()
+    
+       console.log(`Usuário: ${usuarioCarta1.texto} e ${usuarioCarta2.texto}. Pontuação: ${usuarioCarta1.valor + usuarioCarta2.valor}`)
+       console.log(`Computador: ${computadorCarta1.texto} e ${computadorCarta2.texto}. Pontuação: ${computadorCarta1.valor + computadorCarta2.valor}`)
+       if(usuarioCarta1.valor + usuarioCarta2.valor > computadorCarta1.valor + computadorCarta2.valor){
+          console.log(`O usuário ganhou!`)
+    
+       }else if(computadorCarta1.valor + computadorCarta2.valor > usuarioCarta1.valor + usuarioCarta2.valor){
+          console.log(`O computador ganhou!`)
+    
+       }else{
+          console.log(`Empate!`)
+       }
+    }else{
+       console.log(`O jogo acabou! ~Nem começou na verdade kkk~`)
+    
+    }
 
 
 
