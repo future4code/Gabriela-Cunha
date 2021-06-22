b73719733eaf10b02f1d75a5986db1682652d98a
Gabriela Fogaça <gabifogaca.gabriela@gmail.com>
2021-06-21 23:25:50 -0300

    organização

--- revisao/revisaoJS1.js
+++
@@ -1,7 +0,0 @@
-function calculaSalario(qtdeCarrosVendidos, valorTotalVendas) {
-    let porcentagem = 5/100 * valorTotalVendas
-    let salario = 2000 + (qtdeCarrosVendidos * 100) + porcentagem
-    
-    return salario
-  }
-  
